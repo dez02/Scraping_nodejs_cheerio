b35f559dec96141cab3e956bc74e6a1853f36c8a
deutode <deutodesiree@gmail.com>
2018-12-31 11:05:59 +0100

    install feth-node

---
+++ index.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+const url = 'https://www.imdb.com/find?s=tt&ttype=ft&ref_=fn_ft&q=';
+
+function searchMovies(searchTerm){
+  return fetch(`${url}${searchTerm}`)
+  .then(response => response.text());
+}
+
+searchMovies('star wars')
+  .then (body => {
+    console.log(body);
+});
